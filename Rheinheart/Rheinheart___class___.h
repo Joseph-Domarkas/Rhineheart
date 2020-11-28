4d28ed7dc7af7e71e8c33dee372289e575be4bbd
Juozas Domarkas <emilijus_leonardas@icloud.com>
2020-11-28 06:48:08 +0200

    new file:   Rheinheart/Makefile 	new file:   Rheinheart/Rheinheart.c 	new file:   Rheinheart/Rheinheart.h 	new file:   Rheinheart/Rheinheart.iso 	new file:   Rheinheart/Rheinheart___class___.h 	new file:   Rheinheart/a.out 	new file:   Rheinheart/boot.asm 	new file:   Rheinheart/essence/multiboot2.h 	new file:   Rheinheart/iso/boot/Rheinheart.elf 	new file:   Rheinheart/iso/boot/grub/grub.cfg 	new file:   Rheinheart/library/libasm/asm.c 	new file:   Rheinheart/library/libasm/asm.h 	new file:   Rheinheart/library/libstr/string.c 	new file:   Rheinheart/library/libstr/string.h 	new file:   Rheinheart/link.ld 	new file:   Rheinheart/object/Rheinheart.o 	new file:   Rheinheart/object/boot.asm.o

---
+++ Rheinheart/Rheinheart___class___.h
@@ -0,0 +1,18 @@
+
+# ifndef Rheinheart_______Rheinheart__Rheinheart___class______guard__________
+	# define Rheinheart_______Rheinheart__Rheinheart___class______guard__________
+	# define Rheinheart_______Rheinheart__Rheinheart___class______information___name_____________ "Rheinheart class"
+	
+	# define Rheinheart___class___ struct Rheinheart___structure___
+	
+	struct
+	Rheinheart___structure___
+	
+	{
+		
+		long value;
+		
+	};
+
+# endif /* Rheinheart_______Rheinheart__Rheinheart___class______guard__________; */
+
