64af254c0c81b81fc5229feeb15af1ed19e10d7b
Juozas Domarkas <emilijus_leonardas@icloud.com>
2020-11-29 03:47:58 +0200

    deleted:    Rheinheart/Makefile 	deleted:    Rheinheart/Rheinheart.c 	deleted:    Rheinheart/Rheinheart.h 	deleted:    Rheinheart/Rheinheart.iso 	deleted:    Rheinheart/Rheinheart___class___.h 	deleted:    Rheinheart/a.out 	deleted:    Rheinheart/boot.asm 	deleted:    Rheinheart/essence/multiboot2.h 	deleted:    Rheinheart/iso/boot/Rheinheart.elf 	deleted:    Rheinheart/iso/boot/grub/grub.cfg 	deleted:    Rheinheart/library/libasm/asm.c 	deleted:    Rheinheart/library/libasm/asm.h 	deleted:    Rheinheart/library/libstr/string.c 	deleted:    Rheinheart/library/libstr/string.h 	deleted:    Rheinheart/link.ld 	deleted:    Rheinheart/object/Rheinheart.o 	deleted:    Rheinheart/object/boot.asm.o

--- Rheinheart/Rheinheart.h
+++
@@ -1,9 +0,0 @@
-
-# ifndef Rheinheart_______Rheinheart__Rheinheart___guard__________
-	# define Rheinheart_______Rheinheart__Rheinheart___guard__________
-	# define Rheinheart_______Rheinheart__Rheinheart___information___name_____________ "Rheinheart Intel x86_64 operating system"
-	
-	# include <Rheinheart/Rheinheart___class___.h>
-
-# endif /* Rheinheart_______Rheinheart__Rheinheart___guard__________s; */
-
--- Rheinheart/library/libstr/string.h
+++
@@ -1,8 +0,0 @@
-#ifndef STRING_H
-#define STRING_H
-#include <devices/serial/serial.h>
-unsigned int strlen(const char* s);
-char* itoa(int val, int base);
-void printf(char* format, ...);
-char* convert(unsigned int num, int base);
-#endif
